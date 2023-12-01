12f3766b44055cce2367cbd17db150fac860a2bd
C Lorena Sanchez Sua <lorenasua@esquemacreativo.com>
2023-12-02 00:37:49 +0100

    intro config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,20 @@
 export default {
-    srcDir: 'src',
-    // ... otras configuraciones
-  }
+  srcDir: "src",
+  css: ["bootstrap/dist/css/bootstrap.css"],
+  script: [
+    { src: "jquery/dist/jquery.slim.js" },
+    { src: "popper.js/dist/umd/popper.js" },
+    { src: "bootstrap/dist/js/bootstrap.js" },
+  ],
+  build: {
+    extend(config, { isDev, isClient }) {
+      // Agrega la regla para archivos Pug
+      config.module.rules.push({
+        test: /\.pug$/,
+        loader: 'pug-plain-loader',
+        exclude: /node_modules/,
+      });
+    },
+  },
+  // ... otras configuraciones
+};
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,14 @@
 <template>
-    <h1>Hola!</h1>
+    <div class="Intro--page">
+        <h1>Introducing Ahmadiyyat</h1>
+        <h2>The Rennaissanse of Islam</h2>
+        <p>Showcassing Ahmadiyyat as the Rennaissanse of the True Islam, shorn of extremism, irrationality and fanaticism.</p>
+        <p>Founded 1889 by the Imam Mahdi and Muslim Messiah, Mizrah Ghulam Ahmad, peacebe on him.</p>
+        <p>Dedicated to the one God, the truth of Muhammad, the Seal of the prophets, and establishment of world peace.</p>
+        <button> Learn More </button>
+    </div>
 </template>
+
+<style lang="scss" scoped>
+
+</style>
