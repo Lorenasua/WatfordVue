8a329ce47660a4d0324367c76f86d126766ee36a
Camiloruiiz <camiloruiz@esquemacreativo.com>
2024-02-18 05:08:43 +0100

    arreglo

--- tasks/index.js
+++
@@ -1,9 +0,0 @@
-import 'core-js/stable'
-import 'regenerator-runtime/runtime'
-
-import gulp from 'gulp'
-import requireDir from 'require-dir'
-
-requireDir('./', { recurse: true })
-
-export const deploy = gulp.series('rsync')
