4a1a2c7aec743f3cdcd341b621dec2926b11209e
C Lorena Sanchez Sua <lorenasua@esquemacreativo.com>
2024-05-21 22:09:01 +0200

    basic

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,12 +1,25 @@
 <template lang="pug">
-div.intro--page.justify-content-center
-    h1.justify-content-center Introducing Ahmadiyyat
-    h2 The Rennaissanse of Islam
-    p Showcassing Ahmadiyyat as the Rennaissanse of the True Islam, shorn of extremism, irrationality and fanaticism.
-    p Founded 1889 by the Imam Mahdi and Muslim Messiah, Mizrah Ghulam Ahmad, peacebe on him.
-    p Dedicated to the one God, the truth of Muhammad, the Seal of the prophets, and establishment of world peace.
-    button Learn More
+.wrapper--index.m-5
+    .intro--page.d-flex.justify-content-center.flex-column.align-items-center 
+        h1 Introducing Ahmadiyyat
+            h2 The Rennaissanse of Islam
+        .content--text    
+            p Showcassing Ahmadiyyat as the Rennaissanse of the True Islam, shorn of extremism, irrationality and fanaticism. Founded 1889 by the Imam Mahdi and Muslim Messiah, Mizrah Ghulam Ahmad, peacebe on him. Dedicated to the one God, the truth of Muhammad, the Seal of the prophets, and establishment of world peace.
+        button.button--main Learn More
 </template>
 
 <style lang="scss" scoped>
+.button--main {
+  background-color: black;
+  color: white;
+  width: 35%;
+  border-radius: 5px;
+  padding: 5px 0 5px 0;
+}
+
+.content--text {
+  margin: 0 0 50px;
+  //   line-height: 15px;
+  text-align: center;
+}
 </style>
